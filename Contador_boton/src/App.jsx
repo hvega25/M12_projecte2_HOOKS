e5dc6e901cffb4ac21f9c3bc4ef144f468f21848
hvega25 <hvega@jaumebalmes.com>
2023-02-07 15:26:19 +0100

    Cambios

--- Contador_boton/src/App.jsx
+++ Contador_boton/src/App.jsx
@@ -6,51 +6,40 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [colorText, setColorText] = useState('green');
+
+  const [numero, setNumero] = useState(444) //se utiliza para la segunda parte con los tres botones 
+  const [numero1, setNumero1] = useState(0) //se utiliza para la primera parte con el botón increntar
+
+  const Incrementar = () => {
+    setNumero1(numero1 + 1)
+  }
+
+  const Incrementar1 = () => {
+    setNumero(numero + 1)
+  }
+
+  const Decrementar = () => {
+    setNumero(numero - 1)
+  }
+
+  const Reset = () => {
+    setNumero(0)
+  }
 
   return (
-    <div className='App'>
-      <button onClick={() => {
-        setColorText(colorText == 'green' ? 'red' : 'green')
-      }}> cambio
-
-      </button>
-      <h1 style={{ color: colorText }} > Cambio de color</h1>
+    <div className="App">
+      <h1> Primera parte </h1>
+      <button onClick={Incrementar}>Incrementar</button><br />
+      {numero1}
+      <h1> Segunda Parte </h1>
+      <button onClick={Incrementar1}>Incrementar</button>
+      <button onClick={Decrementar}>Decrementar</button>
+      <button onClick={Reset}>Reset</button><br />
+      {numero}
     </div>
   );
-  /* const [numero, setNumero] = useState(444) //se utiliza para la segunda parte con los tres botones 
-   const [numero1, setNumero1] = useState(0) //se utiliza para la primera parte con el botón increntar
- 
-   const Incrementar = () => {
-     setNumero1(numero1 + 1)
-   }
- 
-   const Incrementar1 = () => {
-     setNumero(numero + 1)
-   }
- 
-   const Decrementar = () => {
-     setNumero(numero - 1)
-   }
- 
-   const Reset = () => {
-     setNumero(0)
-   }
- 
-   return (
-     <div className="App">
-       <h1> Primera parte </h1>
-       <button onClick={Incrementar}>Incrementar</button><br />
-       {numero1}
-       <h1> Segunda Parte </h1>
-       <button onClick={Incrementar1}>Incrementar</button>
-       <button onClick={Decrementar}>Decrementar</button>
-       <button onClick={Reset}>Reset</button><br />
-       {numero}
-     </div>
-   );
- 
-   */
+
+
 }
 
 
